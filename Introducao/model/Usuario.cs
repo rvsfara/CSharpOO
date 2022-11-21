225efd10d8ea47eb38ed2e9df81047e6c958c6e9
rvsfara <rvsfara@gmail.com>
2022-11-21 11:15:57 -0300

    Inicio

---
+++ Introducao/model/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introducao.model
+{
+    public class Usuario
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+        public Animal Animal { get; set; }
+
+    }
+}
---
+++ Introducao/teste/TesteUsuarioAnimal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Introducao.model;
+
+namespace Introducao.teste
+{
+    internal class TesteUsuarioAnimal
+    {
+        public static void Main(string[] args)
+        {
+            Usuario Usuario = new Usuario();
+            Animal Animal = new Animal();
+            Usuario.Name = "Rafael Vieira dos Santos";
+            Usuario.Email = "rvsfara@gmail.com";
+            Usuario.Idade = 31;
+
+            Animal.Nome = "Choco";
+            Animal.Especie = "Cachorro";
+            Animal.Idade = 5;
+
+            Usuario.Animal = Animal;
+
+            Console.WriteLine("Iniciando o Programa");
+            Console.WriteLine("Nome: " + Usuario.Name);
+            Console.WriteLine("Email: " + Usuario.Email);
+            Console.WriteLine("Idade: " + Usuario.Idade);
+
+            Console.WriteLine("Pet é um: " + Usuario.Animal.Especie);
+            Console.WriteLine("Nome do pet: " + Usuario.Animal.Nome);
+            Console.WriteLine("Idade do pet: " + Usuario.Animal.Idade);
+            Console.WriteLine("Fim do Programa");
+        }
+    }
+}
